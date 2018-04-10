9dcb1138147376fcb31071ce2ad1750d8eca55b2
Choi <hun5077@gmail.com>
2018-04-11 00:19:07 +0900

    asio synchronous timer

---
+++ asio/synchronous_timer.cpp
@@ -0,0 +1,36 @@
+/*
+ * asio 동기 타이머 사용 예제
+ */
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+#include <boost/asio.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+
+
+void print_timer_function(const boost::system::error_code& /*e*/) {
+	std::cout << "Hello World" << std::endl;
+}
+
+
+
+// 비동기 타이머.
+void timer_wait_test(void) {
+	boost::asio::io_service io;
+	boost::asio::deadline_timer timer(io, boost::posix_time::seconds(2));
+
+	timer.async_wait(&print_timer_function);
+	
+	io.run();
+}
+
+
+
+int main(void) {
+	timer_wait_test();
+
+	return 0;
+}	
